eb6bbc298935228f53e4c0a17882dc5552192df9
Kevin <diasks2@gmail.com>
2013-02-06 16:50:30 +0900

    Add bootstrap and more static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,23 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def app
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def press
+  end
+
+  def video
+  end
+
+  def education
+  end
+
 end

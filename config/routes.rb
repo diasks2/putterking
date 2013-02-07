c935a9e0025fc9ff6cfa145899f40c1c40655fe6
Kevin <diasks2@gmail.com>
2013-02-07 11:39:40 +0900

    Fix contact routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Putterking::Application.routes.draw do
   root to: 'static_pages#home'
   match '/app', to: 'static_pages#app'
   match '/about', to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
   match 'physics', to: 'static_pages#physics'
   match 'math', to: 'static_pages#math'
   match '/home', to: 'static_pages#home'

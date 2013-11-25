db8522d808f987241f384f955d9dcc35f58733b6
Kevin <diasks2@gmail.com>
2013-11-25 19:52:43 +0900

    Update Rails version

--- Gemfile
+++ Gemfile
@@ -1,26 +1,22 @@
 source 'https://rubygems.org'
-
-gem 'rails', '3.2.13'
-gem 'bootstrap-sass', '~> 2.2.2.0'
+ruby '2.0.0'
+gem 'rails', '~> 4.0.1'
+gem 'bootstrap-sass', '2.2.2.0'
 
 group :development, :test do
-  gem 'sqlite3', '1.3.5'
-  gem 'rspec-rails', '2.11.0'
+  gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
+  gem 'railroady'
 end
 
-# Gems used only for assets and not required
-# in production environments by default.
-group :assets do
-  gem 'sass-rails',   '3.2.5'
-  gem 'coffee-rails', '3.2.2'
-  gem 'uglifier', '1.2.3'
-end
-
-gem 'jquery-rails', '2.0.2'
-
-group :test do
-  gem 'capybara', '1.1.2'
-end
+gem 'rails_12factor'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails', '~> 3.0.4'
+gem 'jquery-ui-rails'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
 
 group :production do
   gem 'pg', '0.12.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,44 @@
 # -*- encoding : utf-8 -*-
 Putterking::Application.routes.draw do
   root to: 'static_pages#home'
-  match '/app', to: 'static_pages#app'
-  match '/en/app', to: 'static_pages#app'
-  match '/en/about', to: 'static_pages#about'
-  match '/about', to: 'static_pages#about'
-  match '/en/content/education', to: 'static_pages#math'
-  match '/en/content/education-physics', to: 'static_pages#physics'
-  match '/physics', to: 'static_pages#physics'
-  match '/en/content/education-math', to: 'static_pages#math'
-  match '/math', to: 'static_pages#math'
-  match '/home', to: 'static_pages#home'
-  match '/en/scavenger-hunt/archive', to: 'static_pages#pksh1'
-  match '/pksh1', to: 'static_pages#pksh1'
-  match '/pksh2', to: 'static_pages#pksh2'
-  match '/en/content/top-10-nostalgic-miniature-golf-obstacles', to: 'blogs#nostalgic'
-  match '/blog/top-10-nostalgic-miniature-golf-obstacles', to: 'blogs#nostalgic', :as => 'nostalgic'
-  match '/en/content/putting-away-parkinson’s', to: 'blogs#parkinson'
-  match '/blog/putting-away-parkinson', to: 'blogs#parkinson', :as => 'parkinson'
-  match '/blog/dentist-precision', to: 'blogs#dentist', :as => 'dentist'
-  match '/en/content/dentist’s-precision', to: 'blogs#dentist'
-  match '/en/content/adventures-crazy-world-minigolf-tour', to: 'blogs#crazy'
-  match '/blog/adventures-crazy-world-minigolf-tour', to: 'blogs#crazy', :as => 'crazy'
-  match '/en/content/insight-creativity-and-imagination', to: 'blogs#creativity'
-  match '/blog/insight-creativity-and-imagination', to: 'blogs#creativity', :as => 'creativity'
-  match '/blog/putter-king-scavenger-hunt-riddle', to: 'blogs#riddle', :as => 'riddle'
-  match '/en/content/putter-king-scavenger-hunt-riddle-answer', to: 'blogs#riddle'
-  match '/blog/finger-pulse-minigolf-world', to: 'blogs#pulse', :as => 'pulse'
-  match '/en/content/finger-pulse-minigolf-world', to: 'blogs#pulse'
-  match '/blog/miniature-golf-down-under', to: 'blogs#under', :as => 'under'
-  match '/en/content/miniature-golf-down-under', to: 'blogs#under'
-  match '/blog/greatest-nutter-ever-hold-putter', to: 'blogs#nutter', :as => 'nutter'
-  match '/en/content/greatest-nutter-ever-hold-putter', to: 'blogs#nutter'
-  match '/blog/minigolf-machine', to: 'blogs#machine', :as => 'machine'
-  match '/en/content/minigolf-machine', to: 'blogs#machine'
-  match '/blog/minigolf-ninjas', to: 'blogs#ninjas', :as => 'ninjas'
-  match '/en/content/minigolf-ninjas', to: 'blogs#ninjas'
-  match '/blog/putter-king-adventure-golf-update', to: 'blogs#winners', :as => 'winners'
-  match '/en/content/putter-king-adventure-golf-update', to: 'blogs#winners'
-  match '/blog', to: 'blogs#index'
-  match 'contact' => 'contact#new', :as => 'contact', :via => :get
-  match 'contact' => 'contact#create', :as => 'contact', :via => :post
+  match '/app', to: 'static_pages#app', via: :get
+  match '/en/app', to: 'static_pages#app', via: :get
+  match '/en/about', to: 'static_pages#about', via: :get
+  match '/about', to: 'static_pages#about', via: :get
+  match '/en/content/education', to: 'static_pages#math', via: :get
+  match '/en/content/education-physics', to: 'static_pages#physics', via: :get
+  match '/physics', to: 'static_pages#physics', via: :get
+  match '/en/content/education-math', to: 'static_pages#math', via: :get
+  match '/math', to: 'static_pages#math', via: :get
+  match '/home', to: 'static_pages#home', via: :get
+  match '/en/scavenger-hunt/archive', to: 'static_pages#pksh1', via: :get
+  match '/pksh1', to: 'static_pages#pksh1', via: :get
+  match '/pksh2', to: 'static_pages#pksh2', via: :get
+  match '/en/content/top-10-nostalgic-miniature-golf-obstacles', to: 'blogs#nostalgic', via: :get
+  match '/blog/top-10-nostalgic-miniature-golf-obstacles', to: 'blogs#nostalgic', :as => 'nostalgic', via: :get
+  match '/en/content/putting-away-parkinson’s', to: 'blogs#parkinson', via: :get
+  match '/blog/putting-away-parkinson', to: 'blogs#parkinson', :as => 'parkinson', via: :get
+  match '/blog/dentist-precision', to: 'blogs#dentist', :as => 'dentist', via: :get
+  match '/en/content/dentist’s-precision', to: 'blogs#dentist', via: :get
+  match '/en/content/adventures-crazy-world-minigolf-tour', to: 'blogs#crazy', via: :get
+  match '/blog/adventures-crazy-world-minigolf-tour', to: 'blogs#crazy', :as => 'crazy', via: :get
+  match '/en/content/insight-creativity-and-imagination', to: 'blogs#creativity', via: :get
+  match '/blog/insight-creativity-and-imagination', to: 'blogs#creativity', :as => 'creativity', via: :get
+  match '/blog/putter-king-scavenger-hunt-riddle', to: 'blogs#riddle', :as => 'riddle', via: :get
+  match '/en/content/putter-king-scavenger-hunt-riddle-answer', to: 'blogs#riddle', via: :get
+  match '/blog/finger-pulse-minigolf-world', to: 'blogs#pulse', :as => 'pulse', via: :get
+  match '/en/content/finger-pulse-minigolf-world', to: 'blogs#pulse', via: :get
+  match '/blog/miniature-golf-down-under', to: 'blogs#under', :as => 'under', via: :get
+  match '/en/content/miniature-golf-down-under', to: 'blogs#under', via: :get
+  match '/blog/greatest-nutter-ever-hold-putter', to: 'blogs#nutter', :as => 'nutter', via: :get
+  match '/en/content/greatest-nutter-ever-hold-putter', to: 'blogs#nutter', via: :get
+  match '/blog/minigolf-machine', to: 'blogs#machine', :as => 'machine', via: :get
+  match '/en/content/minigolf-machine', to: 'blogs#machine', via: :get
+  match '/blog/minigolf-ninjas', to: 'blogs#ninjas', :as => 'ninjas', via: :get
+  match '/en/content/minigolf-ninjas', to: 'blogs#ninjas', via: :get
+  match '/blog/putter-king-adventure-golf-update', to: 'blogs#winners', :as => 'winners', via: :get
+  match '/en/content/putter-king-adventure-golf-update', to: 'blogs#winners', via: :get
+  match '/blog', to: 'blogs#index', via: :get
+  match '/contact', to: 'contact#new', :as => :nil, via: :get
+  match '/contact', to: 'contact#create', :as => 'contact', via: :post
 end
